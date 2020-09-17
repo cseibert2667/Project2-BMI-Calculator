780a32d86128437f04e9206e389796fb6e03fa53
Dalton Natale <cdalton.natale@gmail.com>
2020-09-17 11:19:09 -0700

    ajax call to server

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,53 @@
+// const { method } = require("lodash");
+
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
+  $.get("/api/user_data").then((data) => {
     $(".member-name").text(data.email);
   });
 });
+// // AJAX calls
+const bmi = document.querySelector("#bmi-input");
+bmi.onsubmit = function(e) {
+  e.preventDefault();
+  console.log(e.target.elements);
+  let formdata = {
+    weight: parseInt(
+      $("#weight")
+        .val()
+        .trim()
+    ),
+    height: parseInt(
+      $("#height")
+        .val()
+        .trim()
+    ),
+    age: parseInt(
+      $("#age")
+        .val()
+        .trim()
+    ),
+    hip: parseInt(
+      $("#hip")
+        .val()
+        .trim()
+    ),
+    waist: parseInt(
+      $("#waist")
+        .val()
+        .trim()
+    ),
+    sex: $("#sex")
+      .val()
+      .trim(),
+  };
+  const calc = `{weight: { value: "${formdata.weight}", unit: "lb" },height: { value: "${formdata.height}", unit: "ft-in" },sex: { value: "${formdata.sex}", unit: "M" || "F"},age: { value: "${formdata.age}},waist:{ value: "${formata.waist},hip: "40.00",}`;
+  $.ajax({
+    url: "/api/bmi",
+    method: "POST",
+    data: calc,
+  }); //.then;
+  console.log(e.target);
+  return false;
+};
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,22 +10,27 @@ module.exports = function(app) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
-      id: req.user.id
+      id: req.user.id,
     });
   });
-
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // app.post("/api/bmi", (req, res) => {
+  //   console.log(req.body);
+  //   db.Info.create(req.body).then(function(info) {
+  //     res.json(info); // using model to create doc and insert inside db, once we get it back were sending it back to front end
+  //   });
+  // });
+  // // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
     })
       .then(() => {
         res.redirect(307, "/api/login");
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(401).json(err);
       });
   });
@@ -46,7 +51,7 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
       });
     }
   });
