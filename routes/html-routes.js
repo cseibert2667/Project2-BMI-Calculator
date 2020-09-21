e1287f8fefbaa711836fcd51106a9d2ee1d21ff3
cseibert2667 <cseibert2667@gmail.com>
2020-09-21 10:21:22 -0700

    set up handlebars for login/signup pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,24 +1,32 @@
-// Requiring path to so we can use relative routes to our HTML files
+// Requiring express & handlebars 
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+// Require path for the members page
 const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("../public/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("login");
   });
 
-  app.get("/login", (req, res) => {
+  app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("../public/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("signup");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
